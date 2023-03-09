366eedd0dc36ceafcb5f5ec8517ecdfc73b33ace
singularitti <singularitti@outlook.com>
2023-03-08 23:09:04 -0500

    Add names & more options to `WorkflowPlot`

--- src/SimpleWorkflowRecipes.jl
+++ src/SimpleWorkflowRecipes.jl
@@ -6,6 +6,13 @@ using RecipesBase: @userplot, @recipe
 @userplot WorkflowPlot
 @recipe function f(plot::WorkflowPlot)
     workflow = plot.args[end]
+    curvature_scalar --> 0.01
+    root := :bottom
+    nodeshape --> :ellipse
+    nodesize --> 0.2
+    names --> map(job -> getfield(job, :name), workflow.jobs)
+    fontsize --> 9
+    method --> :spring
     return GraphPlot(get_source_destiny_weight(get_adjacency_list(workflow.graph)))
 end
 
