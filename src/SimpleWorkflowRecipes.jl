2d2082f7627ac7a593be728d31a4afb075577803
singularitti <singularitti@outlook.com>
2023-03-08 22:32:32 -0500

    Add type `WorkflowPlot`

--- src/SimpleWorkflowRecipes.jl
+++ src/SimpleWorkflowRecipes.jl
@@ -1,5 +1,12 @@
 module SimpleWorkflowRecipes
 
-# Write your package code here.
+using GraphRecipes: GraphPlot, get_source_destiny_weight, get_adjacency_list
+using RecipesBase: @userplot, @recipe
+
+@userplot WorkflowPlot
+@recipe function f(plot::WorkflowPlot)
+    workflow = plot.args[end]
+    return GraphPlot(get_source_destiny_weight(get_adjacency_list(workflow.graph)))
+end
 
 end
